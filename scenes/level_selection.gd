14a97090220b1c38ebf4809f33623138ec7d20b2
OmkarManerikar <omkarmanerikar1708@gmail.com>
2025-03-23 20:33:37 +0530

    level 2 script

--- scenes/level_selection.gd
+++ scenes/level_selection.gd
@@ -13,3 +13,7 @@ func _process(delta: float) -> void:
 
 func _on_level_1_button_down() -> void:
 	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+
+
+func _on_level_2_button_down() -> void:
+	get_tree().change_scene_to_file("res://scenes/level_2.tscn")
