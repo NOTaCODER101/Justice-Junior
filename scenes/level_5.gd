01ded15dadbb0532ad84949e3d21b2a21031fa09
OmkarManerikar <omkarmanerikar1708@gmail.com>
2025-05-06 22:23:36 +0530

    level 5

--- scenes/level_5.gd
+++ scenes/level_5.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	Dialogic.start("level_4_timeline")
+	Dialogic.start("level_5_timeline")
 	Dialogic.timeline_ended.connect(_on_timeline_ended)
 
 
